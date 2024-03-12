8fb9ad1d1c20ab71941ea8cb0c0b6080acf77f17
kazim110 <kazim.mohammadi110@gmail.com>
2024-03-12 21:52:09 +0430

    fix linter error

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
-import greetingReducer from './reducers/GreetingReducer'
+import greetingReducer from './reducers/GreetingReducer';
 
 const store = configureStore({
   reducer: {
