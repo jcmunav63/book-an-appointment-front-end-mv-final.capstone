9f8084f6bbbbb3064521d2fa9b608fdde6882f3a
kazim <kazim.mohammadi110@gmail.com>
2024-03-14 13:57:23 +0430

    add some style for splash screen

--- src/splash.js
+++ src/splash.js
@@ -4,8 +4,12 @@ import './splash.css';
 function Splash() {
   return (
     <div className="splash-screen">
-      <div className="overlay">
-        <h1>Welcome to My App</h1>
+      <div className="splash-overlay">
+        <h1 className="splash-welcome">Alpha Reservations</h1>
+        <div className="splash-btn-container">
+          <button type="button" className="splash-login-btn">Log In</button>
+          <button type="button" className="splash-signup-btn">Sing Up</button>
+        </div>
       </div>
     </div>
   );
