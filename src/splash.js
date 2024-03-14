f1f84257bb6a76cd4393c9fdb768e6ee97a84333
kazim <kazim.mohammadi110@gmail.com>
2024-03-14 11:20:11 +0430

    add splash screen files

---
+++ src/splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './splash.css'; // Import CSS for styling
+
+function Splash() {
+  return (
+    <div className="splash-screen">
+      <h1>Welcome to My App</h1>
+      {/* Add any other content or branding here */}
+    </div>
+  );
+}
+
+export default Splash;
