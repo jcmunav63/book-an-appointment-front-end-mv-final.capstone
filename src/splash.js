7e19b132da410d4e4dedc7817fd8c24a0de8e462
kazim <kazim.mohammadi110@gmail.com>
2024-03-14 12:17:33 +0430

    Add background and its overlay

--- src/splash.js
+++ src/splash.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './splash.css'; // Import CSS for styling
+import './splash.css';
 
 function Splash() {
   return (
     <div className="splash-screen">
-      <h1>Welcome to My App</h1>
-      {/* Add any other content or branding here */}
+      <div className="overlay">
+        <h1>Welcome to My App</h1>
+      </div>
     </div>
   );
 }
