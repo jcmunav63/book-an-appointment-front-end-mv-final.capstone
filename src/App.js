7a0866a4e599fed1c85872b10c7879dba6d15076
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2024-03-15 20:43:17 +0430

    Update 2 App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Users from './components/Users';
+{/* import Users from './components/Users'; */}
 import Login from './components/LoginComponent';
 import './App.css';
 import Register from './components/RegisterComponent';
@@ -13,9 +13,8 @@ function App() {
         <Splash />
         <Routes>
           <Route exact path="/" element={<Login />} />
-//           <Route exact path="/users" element={<Users />} />
+          {/* <Route exact path="/users" element={<Users />} /> */}
           <Route exact path="/register" element={<Register />} />
-
           {/* More routes here if needed */}
           <Route component={() => <div>404 Not Found</div>} />
         </Routes>
