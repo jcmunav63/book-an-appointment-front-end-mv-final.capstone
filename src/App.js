521a7c2be7893b9729095431bb6033e2b4917439
kazim110 <kazim.mohammadi110@gmail.com>
2024-03-15 20:55:02 +0430

    fix linter error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-{/* import Users from './components/Users'; */}
+// import Users from './components/Users';
 import Login from './components/LoginComponent';
 import './App.css';
 import Register from './components/RegisterComponent';
