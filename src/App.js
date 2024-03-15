448a81e06d388466893edf0d5e92810aadb328f4
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-14 21:31:11 -0600

    Add new components as routes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Users from './components/Users';
+import Login from './components/LoginComponent';
 import './App.css';
+import Register from './components/RegisterComponent';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path="/" element={<Users />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/users" element={<Users />} />
+          <Route exact path="/register" element={<Register />} />
+
           {/* More routes here if needed */}
           <Route component={() => <div>404 Not Found</div>} />
         </Routes>
