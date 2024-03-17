3e7e6de2305e22091620dac6e1dbd20812f8a733
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-16 22:31:09 -0600

    Feat: Add protected routes.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Users from './components/Users';
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router, Route, Routes, Navigate,
+} from 'react-router-dom';
+import Users from './components/Users';
 import Login from './components/LoginComponent';
 import './App.css';
 import Register from './components/RegisterComponent';
 import Splash from './splash';
 
+const PrivateRoute = ({ children }) => {
+  const userString = localStorage.getItem('user');
+  const user = userString ? JSON.parse(userString) : null;
+  return user ? children : <Navigate to="/" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 function App() {
   return (
     <Router>
       <div>
-        <Splash />
         <Routes>
-          <Route exact path="/" element={<Login />} />
-          {/* <Route exact path="/users" element={<Users />} /> */}
+          <Route exact path="/" element={<Splash />} />
+          <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
+          <Route exact path="/users" element={<PrivateRoute><Users /></PrivateRoute>} />
           {/* More routes here if needed */}
           <Route component={() => <div>404 Not Found</div>} />
         </Routes>
