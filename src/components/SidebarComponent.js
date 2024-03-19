2647b2218d0931c6ea81d7be81858998b9f9cfdc
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-19 14:36:06 -0600

    Refactor: Sidebar to navigate routes.

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../assets/stylesheets/sidebar.module.css';
 
 function Sidebar() {
+  const navigate = useNavigate();
   const [isActive, setIsActive] = React.useState(false);
 
   const clickHandle = () => {
@@ -12,10 +14,12 @@ function Sidebar() {
     <>
       <div className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
+          <li><button type="button" onClick={() => navigate('/home')}>Home</button></li>
+          <li><button type="button" onClick={() => navigate('/NewReservation')}>Reserve</button></li>
+          <li><button type="button" onClick={() => navigate('#')}>My Reservations</button></li>
+          <li><button type="button" onClick={() => navigate('/newSpaceCw')}>Add coworking</button></li>
+          <li><button type="button" onClick={() => navigate('/deleteSpaceCw')}>Delete coworking</button></li>
+          <li><button type="button" onClick={() => navigate('#')}>Logout</button></li>
         </ul>
       </div>
       <button type="button" className={`${styles.icon} ${isActive ? styles.closeIcon : ''}`} onClick={clickHandle} aria-label="Toggle Sidebar" />
