c452f2d3d3b3f5ccfbe1e9fb1bcc5769eef96591
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-18 21:12:53 -0600

    Fix: Import for redux store.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './reducers/index';
+import store from './reducers/store';
 import './index.css';
 import App from './App';
 
