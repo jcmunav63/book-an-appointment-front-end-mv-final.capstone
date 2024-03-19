305cb2b3c333cc36a50a1afc5d25f826a6c6bb05
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-18 21:11:54 -0600

    Chore: Delete unnecessary file.

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
-
-// eslint-disable-next-line import/prefer-default-export
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-export default store;
