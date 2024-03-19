164e070dedf5d3f864718b71fb18aee37a088355
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-19 16:07:03 -0600

    Style: Home page and sidebar to match design.

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -13,13 +13,14 @@ function Sidebar() {
   return (
     <>
       <div className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
+        <h1>SMART COWORKING</h1>
         <ul>
-          <li><button type="button" onClick={() => navigate('/home')}>Home</button></li>
-          <li><button type="button" onClick={() => navigate('/NewReservation')}>Reserve</button></li>
-          <li><button type="button" onClick={() => navigate('#')}>My Reservations</button></li>
-          <li><button type="button" onClick={() => navigate('/newSpaceCw')}>Add coworking</button></li>
-          <li><button type="button" onClick={() => navigate('/deleteSpaceCw')}>Delete coworking</button></li>
-          <li><button type="button" onClick={() => navigate('#')}>Logout</button></li>
+          <li><button type="button" onClick={() => navigate('/home')}>HOME</button></li>
+          <li><button type="button" onClick={() => navigate('/NewReservation')}>RESERVE</button></li>
+          <li><button type="button" onClick={() => navigate('#')}>RESERVATIONS</button></li>
+          <li><button type="button" onClick={() => navigate('/newSpaceCw')}>ADD</button></li>
+          <li><button type="button" onClick={() => navigate('/deleteSpaceCw')}>DELETE</button></li>
+          <li><button type="button" onClick={() => navigate('#')}>LOGOUT</button></li>
         </ul>
       </div>
       <button type="button" className={`${styles.icon} ${isActive ? styles.closeIcon : ''}`} onClick={clickHandle} aria-label="Toggle Sidebar" />
