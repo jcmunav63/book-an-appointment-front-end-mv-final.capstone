455845c85368d00019d51fa4184dbcdac5ef038d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-20 15:04:10 -0600

    Feat: Add Details page and install redux-persist.

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import '../assets/css/details-page.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchCoworkingSpace } from '../actions/homeActions';
 
 const DetailsPage = () => {
-  const { loading, error } = useSelector(
-    (state) => state.coworkingSpaces,
-  );
+  const dispatch = useDispatch();
+  const { spaceId } = useParams();
+  const { coworkingSpaces, loading, error } = useSelector((state) => state.coworkingSpaces);
+
+  const spaceDetails = coworkingSpaces && Array.isArray(coworkingSpaces)
+    ? coworkingSpaces.find((space) => space.id.toString() === spaceId)
+    : null;
+
+  useEffect(() => {
+    if (!spaceDetails) {
+      dispatch(fetchCoworkingSpace(spaceId));
+    }
+  }, [dispatch, spaceId, spaceDetails]);
+
   return (
     <div className="details-page-container">
-      <div className="details-page-content-center">
-        {loading && <p>Loading...</p>}
-        {error && (
-        <p>
-          Error:
-          {error}
-        </p>
-        )}
-        <h1>Details page</h1>
-      </div>
+      {loading && <p>Loading...</p>}
+      {error && (
+      <p>
+        Error:
+        {error}
+      </p>
+      )}
+      {spaceDetails && (
+        <div className="details-content">
+          <h1>{spaceDetails.name}</h1>
+          <img className="space-image" alt={spaceDetails.name} src={spaceDetails.image} />
+          <p>{spaceDetails.description}</p>
+        </div>
+      )}
     </div>
   );
 };
