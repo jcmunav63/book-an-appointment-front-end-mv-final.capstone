64a5a9bc6204d39779e36293ed63d45f062afaa2
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-20 17:16:38 -0600

    Fix: Move splash page to correct place.

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './splash.css';
+import '../assets/css/splash.css';
 
 function Splash() {
   const navigate = useNavigate();
