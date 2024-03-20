22a024e50820dcf0366929799577d555f80dc0ea
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-20 15:04:53 -0600

    Feat: Fetch specific coworking space.

--- src/actions/homeActions.js
+++ src/actions/homeActions.js
@@ -19,3 +19,23 @@ export const fetchCoworkingSpaces = (userId) => async (dispatch) => {
     });
   }
 };
+
+export const FETCH_COWORKING_SPACE_REQUEST = 'FETCH_COWORKING_SPACE_REQUEST';
+export const FETCH_COWORKING_SPACE_SUCCESS = 'FETCH_COWORKING_SPACE_SUCCESS';
+export const FETCH_COWORKING_SPACE_FAILURE = 'FETCH_COWORKING_SPACE_FAILURE';
+
+export const fetchCoworkingSpace = (spaceId) => async (dispatch) => {
+  dispatch({ type: FETCH_COWORKING_SPACE_REQUEST });
+  try {
+    const response = await axios.get(`http://127.0.0.1:3001/api/v1/space_cws/${spaceId}`);
+    dispatch({
+      type: FETCH_COWORKING_SPACE_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_COWORKING_SPACE_FAILURE,
+      payload: error.message,
+    });
+  }
+};
