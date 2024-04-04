d9e622db190627bbdd719ca3c3eca9c208cbb463
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-04-04 15:34:02 -0600

    Fix: Fix sid bar.

--- src/components/SidebarComponent.js
+++ src/components/SidebarComponent.js
@@ -7,6 +7,7 @@ import styles from '../assets/stylesheets/sidebar.module.css';
 function Sidebar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const userId = JSON.parse(localStorage.getItem('user'))?.user.id;
   const [isActive, setIsActive] = React.useState(false);
 
   const navigateTo = (route) => {
@@ -44,14 +45,16 @@ function Sidebar() {
     <>
       <div className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
         <h1>SMART COWORKING</h1>
-        <ul>
-          <li><button type="button" onClick={() => navigateTo('/home')}>HOME</button></li>
-          <li><button type="button" onClick={() => navigateTo('/NewReservation')}>RESERVE</button></li>
-          <li><button type="button" onClick={() => navigateTo('/MyReservations')}>RESERVATIONS</button></li>
-          <li><button type="button" onClick={() => navigateTo('/newSpaceCw')}>ADD</button></li>
-          <li><button type="button" onClick={() => navigateTo('/deleteSpaceCw')}>DELETE</button></li>
-          <li><button type="button" onClick={handleLogout}>LOGOUT</button></li>
-        </ul>
+        {userId && (
+          <ul>
+            <li><button type="button" onClick={() => navigateTo('/home')}>HOME</button></li>
+            <li><button type="button" onClick={() => navigateTo('/NewReservation')}>RESERVE</button></li>
+            <li><button type="button" onClick={() => navigateTo('/MyReservations')}>RESERVATIONS</button></li>
+            <li><button type="button" onClick={() => navigateTo('/newSpaceCw')}>ADD</button></li>
+            <li><button type="button" onClick={() => navigateTo('/deleteSpaceCw')}>DELETE</button></li>
+            <li><button type="button" onClick={handleLogout}>LOGOUT</button></li>
+          </ul>
+        )}
       </div>
       <button type="button" className={`${styles.icon} ${isActive ? styles.closeIcon : ''}`} onClick={clickHandle} aria-label="Toggle Sidebar" />
     </>
