33d2fecf837a2c43cf95a8778036fea12a01fd7f
Julio Quezada <julio.quezada@catolica.edu.sv>
2024-04-05 23:14:36 -0600

    Update: splash.js to include author.

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -9,6 +9,7 @@ function Splash() {
     <div className="splash-screen">
       <div className="splash-overlay">
         <h1 className="splash-welcome">Smart Coworking</h1>
+        <p className="splash-welcome-paragraph">by Julio Quezada</p>
         <div className="splash-btn-container">
           <button type="button" className="splash-login-btn" onClick={() => navigate('/login')}>Log In</button>
           <button type="button" className="splash-signup-btn" onClick={() => navigate('/register')}>Sign Up</button>
